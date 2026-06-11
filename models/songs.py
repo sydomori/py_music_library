c2b636105f1eca9d989a6f83032b19c445627cd1
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-06-11 22:36:26 +0300

    Feat:Add attribute genre in both class of song and album

--- models/songs.py
+++ models/songs.py
@@ -1,17 +1,19 @@
 class Song:
-    def __init__(self, title, artist, duration):
+    def __init__(self, title, genre,artist, duration):
         self.title = title
+        self.genre = genre
         self.artist = artist
         self.duration = duration
         self.albums=[]#list to track albums this song is apart of.
 
     def __str__(self):
-        return f"{self.title} by {self.artist},duration:{self.duration } mins"
+        return f"{self.title} by {self.artist},in genre : {self.genre} duration:{self.duration } mins"
     
     def to_dict(self):
         return{
 
             "title":self.title,
+            "genre":self.genre,
             "artist":self.artist,
             "duration":self.duration
 
@@ -20,3 +22,6 @@ class Song:
         if album not in self.albums:
             self.albums.append(album)
             album.add_song(self)
+
+    def __repr__(self):
+        return f"Song: {self.title} by {self.artist}, in genre: {self.genre} ,duration:{self.duration}"
