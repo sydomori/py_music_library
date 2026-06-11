065f4b740b6c3d058db4b33b1820a26602b98eb5
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-06-11 22:36:26 +0300

    Feat:Add attribute genre in both class of song and album

--- models/albums.py
+++ models/albums.py
@@ -1,7 +1,8 @@
 class Album:
-    def __init__(self, title, artist, release_year):
+    def __init__(self, title, artist,genre, release_year):
         self.title = title
         self.artist = artist
+        self.genre = genre
         self.release_year = release_year
         self.songs = []#list to track songs in this album.
 
@@ -17,8 +18,9 @@ class Album:
         return{
             "title":self.title,
             "artist":self.artist,
+            "genre":self.genre,
             "release_year":self.release_year,
             "songs":[song.title for song in self.songs]
         }
     def __repr__(self):
-        return f"Song: {self.title} by {self.artist},released in {self.release_year}"
+        return f"Album: {self.title} by {self.artist}, in genre: {self.genre} released in {self.release_year}"
