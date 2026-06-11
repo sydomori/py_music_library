4a97c118405de553efca2d2c089233d2733524f9
daudikombe <daudikombe77@gmail.com>
2026-06-11 22:52:46 +0300

    feat:defining the class and using init constructor

--- models/user.py
+++ models/user.py
@@ -1,1 +1,13 @@
 from models.person import Person
+
+class User(Person):
+    def __init__(self, name, email, user_id):
+        super().__init__(name, email)
+        self._user_id = user_id
+        self.favourite_songs = []
+        self.favourite_albums = []
+
+    def add_favourite_song(self, song, _quiet=False):
+        
+
+        
