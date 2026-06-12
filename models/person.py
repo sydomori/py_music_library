df979e8a14ce3ab44f5ba5e1a2b1bd3e46d9d243
daudikombe <daudikombe77@gmail.com>
2026-06-12 10:26:56 +0300

    feat:decorator for definng grtter method for name prop

--- models/person.py
+++ models/person.py
@@ -1,9 +1,9 @@
 class Person:
-    """Base class for all people in the system."""
-
     def __init__(self, name, email):
         self._name = name
         self._email = email
 
-    def __str__(self):
-        return f"{self._name} ({self._email})"    
+    @property
+    def name(self):
+        return self._name
+    
