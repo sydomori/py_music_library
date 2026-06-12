dcbe010a5863c86745ff453a2d8f229b7a69bf91
sydomori <sydneyomori@gmail.com>
2026-06-12 10:37:35 +0300

    create import statements

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+from my_models.artist2 import Artist
+from my_models.song2 import Song
+from my_models.album2 import Album
+from my_models.user2 import User
+from functions import (
+    create_artist, list_all_artists,
+    create_song, list_all_songs,
+    create_album, list_all_albums,
+    create_user, list_all_users,
+    add_favorite_song, add_favorite_album,
+    view_favorites,
+    search_by_artist, search_by_genre
+)
