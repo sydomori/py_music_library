3b1c9cf5bb9b24eba34599f017b9e57d89c6ed9b
daudikombe <daudikombe77@gmail.com>
2026-06-12 10:01:11 +0300

    feat:using init constructor to automacally run code

--- models/user.py
+++ models/user.py
@@ -3,3 +3,13 @@ from models.person import Person
 class User(Person):
     #store all users
     all_users = []
+    
+    def __init__(self,name,email):
+        super().__init__(name,email)
+        self._favorite_songs = []
+        self._favorite_albums = []
+        User.all_users.append(self)
+
+
+
+
